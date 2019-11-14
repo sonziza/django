e8ef896f565d84d62ccfdd89cbff00b5475951fa
sonziza <sonziza@yandex.ru>
2019-11-14 22:56:14 +0300

    Создал приложение для загадок riddles

--- OlegSite/urls.py
+++ OlegSite/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(r'riddles/', include('riddles.urls')),
     path('blog/', include('blog.urls', namespace = 'blog'))
 ]
---
+++ riddles/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Option, Riddle
+# Register your models here.
+
+admin.site.register(Riddle)
+admin.site.register(Option)
